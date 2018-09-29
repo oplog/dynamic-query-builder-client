fc817f686b3447c0b2067dc245e89b2d4514fb4f
Emre Keskin <emrekeskinmac@gmail.com>
2018-09-30 00:24:14 +0300

    NumericFilter unit test

--- src/test/NumericFilter.ts
+++ src/test/NumericFilter.ts
@@ -1,33 +1,52 @@
 import * as test from 'tape';
+import * as parser from 'query-string-parser';
 import { NumericFilter, NumericFilterOperation } from '..';
 
 test('build query with numeric filter', function(t) {
   t.plan(1);
-  t.equal(new NumericFilter({
-      property: 'age',
-      value: 25,
-    }).build(), 'o=Equals&p=age&v=25');
+  const query = new NumericFilter({
+    property: 'age',
+    value: 25,
+  }).build();
+  t.same(parser.fromQuery(query),  { o: 'Equals', p: 'age', v: '25' });
 });
 
 test('build query with numeric filter for array', function(t) {
   t.plan(1);
-  t.equal(new NumericFilter({
-      property: 'age',
-      value: [25, 26],
-    }).build(), 'o=Equals&p=age&v=25,26');
+  const query = new NumericFilter({
+    property: 'age',
+    value: [25, 26],
+  }).build();
+  t.same(parser.fromQuery(query), { o: 'Equals', p: 'age', v: '25,26' });
+});
+
+test('build query with numeric filter for valueToString', function(t) {
+  t.plan(1);
+  const query = new NumericFilter({
+    property: 'age',
+    value: [25, 26],
+  }).valueToString();
+  t.same(query, '25,26');
+});
+
+test('build query with numeric filter for valueToString', function(t) {
+  t.plan(1);
+  const query = new NumericFilter({
+    property: 'age',
+    value: 25,
+  }).valueToString();
+  t.same(query, '25');
 });
 
 test('build query with numeric filter for every operations', function(t) {
   const operations = Object.keys(NumericFilterOperation);
   t.plan(operations.length);
   operations.forEach((operation: string) => {
-    t.equal(
-      new NumericFilter({
-        property: 'age',
-        value: 25,
-        op: <NumericFilterOperation>operation,
-      }).build(),
-      `o=${operation}&p=age&v=25`
-    );
+    const query = new NumericFilter({
+      property: 'age',
+      value: 25,
+      op: <NumericFilterOperation>operation,
+    }).build();
+    t.same(parser.fromQuery(query), { o: operation, p: 'age', v: '25' });
   });
 });
