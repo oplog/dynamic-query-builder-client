740b84ff90a98306afed42e787492e1fa0dfaf4e
Uğur Oruç <uoruc5@gmail.com>
2019-07-06 01:22:46 +0300

    Pagination minor fix with missing tests

--- __tests__/Pagination.test.ts
+++ __tests__/Pagination.test.ts
@@ -1,5 +1,5 @@
 import * as parser from "query-string-parser";
-import { Pagination } from "../src/Pagination";
+import { Pagination, DEFAULT_PAGINATION_COUNT } from "../src/Pagination";
 
 describe("Pagination", () => {
   it("build query with pagination filter", () => {
@@ -11,5 +11,29 @@ describe("Pagination", () => {
       offset: "0",
       count: "10",
     });
+  });
+
+  it("should use default pagination count", () => {
+    let count = new Pagination({
+      offset: 0,
+    }).count;
+
+    expect(count).toBe(DEFAULT_PAGINATION_COUNT);
+  });
+
+  it("should get current page", () => {
+    let currentPage = new Pagination({
+      offset: 25,
+      count: 10,
+    }).currentPage;
+
+    expect(currentPage).toEqual(3);
+
+    currentPage = new Pagination({
+      offset: 30,
+      count: 10,
+    }).currentPage;
+
+    expect(currentPage).toEqual(4);
   });
 });
