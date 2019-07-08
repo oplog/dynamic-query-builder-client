1209c4a14bab581c86163b3d6d56ee63ec8091cb
Uğur Oruç <uoruc5@gmail.com>
2019-07-09 00:12:47 +0300

    package update

--- __tests__/ArrayFilter.test.ts
+++ __tests__/ArrayFilter.test.ts
@@ -1,5 +1,5 @@
 import { ArrayFilter } from "../src/ArrayFilter";
-import { ArrayFilterOperation } from "../src/FilterOperation";
+import { ArrayFilterOperation, StringFilterOperation } from "../src/FilterOperation";
 
 // Url Parser is not working in this situation
 // it cannot be able to parse "o=Any&p=Tests&v=(o=contains&p=Name&v=1,2,3,4,5,6)"
@@ -8,8 +8,10 @@ describe("Array Filter", () => {
   it("build query with array filter", () => {
     const query = new ArrayFilter({
       property: "array",
-      op: ArrayFilterOperation.In,
+      op: StringFilterOperation.In,
       value: "123,456",
+      outerOp: ArrayFilterOperation.Any,
+      searchField: "_"
     }).build();
 
     expect(query).toEqual("o=Any&p=array&v=(o=In&p=_&v=123,456)");
@@ -18,36 +20,25 @@ describe("Array Filter", () => {
   it("should build query with array filter for valueToString", () => {
     const query = new ArrayFilter({
       property: "array",
-      op: ArrayFilterOperation.In,
-      value: "123,234,345",
+      op: StringFilterOperation.In,
+      value: "123,456",
+      outerOp: ArrayFilterOperation.Any,
+      searchField: "_"
     }).valueToString();
 
-    expect(query).toEqual("123,234,345");
+    expect(query).toEqual("123,456");
   });
 
   it("should build inner filter for array filter", () => {
     const innerFilter = new ArrayFilter({
       property: "array",
-      op: ArrayFilterOperation.In,
-      value: "123,234,345",
+      op: StringFilterOperation.In,
+      value: "123,456",
+      outerOp: ArrayFilterOperation.Any,
+      searchField: "_"
     }).innerFilterBuild;
 
-    expect(innerFilter).toEqual(`(o=${ArrayFilterOperation.In}&p=_&v=123,234,345)`);
-  });
-
-  it("should build query with array filter for every operations", () => {
-    // Any is just for inside usage
-    const operations = Object.keys(ArrayFilterOperation)
-      .filter(e => e !== ArrayFilterOperation.Any);
-    operations.forEach((operation: ArrayFilterOperation) => {
-      const query = new ArrayFilter({
-        property: "array",
-        op: operation,
-        value: "123,234,345",
-      }).build();
-
-      expect(query).toEqual(`o=Any&p=array&v=(o=${operation}&p=_&v=123,234,345)`);
-    });
+    expect(innerFilter).toEqual(`(o=${StringFilterOperation.In}&p=_&v=123,456)`);
   });
 
 });
